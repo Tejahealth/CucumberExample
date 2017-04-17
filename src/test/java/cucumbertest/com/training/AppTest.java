25e12dc4f74c475a6551e01a28444c278183e752
gsant <gsant@DESKTOP-8V32ODS>
2017-04-17 19:49:47 +0100

    Second change

--- src/test/java/cucumbertest/com/training/AppTest.java
+++ src/test/java/cucumbertest/com/training/AppTest.java
@@ -17,7 +17,7 @@ public class AppTest
      */
     public AppTest( String testName )
     {
-        super( testName );
+        super( testName ) ;
     }
 
     /**
@@ -26,6 +26,8 @@ public class AppTest
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
+        
+        
     }
 
     /**
